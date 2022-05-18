0df82ccbe717ca12ef40fccf948bf16ab1e707d9
Jefferson Veiga <gelsvsampa@hotmail.com>
2022-05-17 21:59:16 -0300

    Apresentando View Components

---
+++ MinhaDemoMvc/ViewComponents/CarrinhoViewComponents.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MinhaDemoMvc.ViewComponents
+{
+    [ViewComponent(Name = "Carrinho")] // Apelido para a View Component 
+    public class CarrinhoViewComponents : ViewComponent
+    {
+        public int ItensCarrinho { get; set; }
+
+        public CarrinhoViewComponents() // Construtor exemplo
+        {
+            ItensCarrinho = 3;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() // Método para chamada da View Component
+        {
+            return View(ItensCarrinho); // Passando os dados para a View a ser chamada
+        }
+    }
+}
